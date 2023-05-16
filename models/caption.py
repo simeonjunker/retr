1221136aac01077826dea9fc31d7f3c0589db6dc
Simeon Schüz <simeon.schuez@uni-bielefeld.de>
2023-05-16 16:16:14 +0200

    reshape scene context

--- models/caption.py
+++ models/caption.py
@@ -132,7 +132,7 @@ class CaptionGlobalLoc(nn.Module):
         g_src, g_mask = g_features.decompose()
         g_src = self.input_proj(g_src)
         assert g_mask is not None
-        # ensure there are unmasked values in the context
+        # ensure there are unmasked context values
         g_mask = ensure_unmasked_values(g_mask)
         # flatten vectors
         g_src = g_src.flatten(2)
@@ -148,15 +148,16 @@ class CaptionGlobalLoc(nn.Module):
 class CaptionSceneLoc(nn.Module):
 
     def __init__(self, backbone, transformer, positional_encoding, hidden_dim,
-                 vocab_size):
+                 vocab_size, scene_dim=134):
         super().__init__()
         self.backbone = backbone
         self.positional_encoding = positional_encoding
+        self.scene_dim = scene_dim
         self.input_proj = nn.Conv2d(in_channels=backbone.num_channels,
                                     out_channels=hidden_dim,
                                     kernel_size=1)
         self.loc_proj = nn.Linear(7, hidden_dim)
-        self.scene_proj = nn.Linear(134, hidden_dim)
+        self.scene_emb = nn.Embedding(self.scene_dim, hidden_dim)
         self.transformer = transformer
         self.mlp = MLP(hidden_dim, 512, vocab_size, 3)
 
@@ -179,16 +180,23 @@ class CaptionSceneLoc(nn.Module):
             (loc_feats.shape[0], 1)).bool().to(t_mask.device)
 
         # concatenate target and location to target vector
-        src = torch.concat([t_src, loc_src], 2)
-        mask = torch.concat([t_mask, loc_masks], 1)
+        target_src = torch.concat([t_src, loc_src], 2)
+        target_mask = torch.concat([t_mask, loc_masks], 1)
 
         # scene features
-        s_src = self.scene_proj(s_features).unsqueeze(-1)
-        s_mask = torch.zeros(
-            (s_features.shape[0], 1)).bool().to(t_mask.device)
+        b, n_feats = s_features.size()
+        
+        s_idx = torch.arange(0, self.scene_dim).repeat((b, 1))  # long tensor with scene feature idx -> [b, n_feats]
+        s_emb_out = self.scene_emb(s_idx)  # embeddings for scene feature idx -> [b, n_feats, hidden_dim]
+
+        weights = s_features.unsqueeze(-1)  # use input features as weights -> [b, n_feats, 1]
+        s_src = torch.mul(s_emb_out, weights)  # elementwise multiplication -> [b, n_feats, hidden_dim]
+        s_src = s_src.permute(0,2,1)  # -> [b, hidden_dim, n_feats]
+
+        s_mask = torch.zeros((b, n_feats), dtype=bool).to(t_mask.device)  # mask with only False values -> [b, n_feats]
 
         hs = self.transformer(
-            src_t=src, mask_t=mask, 
+            src_t=target_src, mask_t=target_mask, 
             src_c=s_src, mask_c=s_mask, 
             tgt=target_exp, tgt_mask=target_exp_mask)
         out = self.mlp(hs.permute(1, 0, 2))
